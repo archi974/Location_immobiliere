7dd0c1d91ee999d46cd2558ae8fe753783bc9d6b
Vincent <vincent.k.97480@gmail.com>
2023-06-14 19:04:51 +0200

    [DELETE/UPDATE] empty file and 404 page

--- frontEnd/src/pages/404.js
+++ frontEnd/src/pages/404.js
@@ -3,7 +3,11 @@ import Layout from '../components/layout';
 export default function NotFoundPage() {
     return (
         <Layout>
-            <p>404</p>
+            <div className="not-found">
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <a href="/">Retourner sur la page d'accueil</a>
+            </div>
         </Layout>
     )
 }
