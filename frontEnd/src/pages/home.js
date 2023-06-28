ae00fb3f911776551309b77f6c8faa61016f83d7
Vincent <vincent.k.97480@gmail.com>
2023-06-28 17:51:05 +0400

    [FIX] header marge

--- frontEnd/src/pages/home.js
+++ frontEnd/src/pages/home.js
@@ -9,8 +9,10 @@ export default function Home() {
 
     return (
         <Layout>
-            <Banner title={titleHome} imgSrc={imgHome} imgAlt={altHome}/>
-            <LocationCard />
+            <div className="home">
+                <Banner title={titleHome} imgSrc={imgHome} imgAlt={altHome} />
+                <LocationCard />
+            </div>
         </Layout>
     )
 }
